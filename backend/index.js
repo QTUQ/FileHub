ca551d486618819777cc7d41777f56b359d4e86c
QTUQ <tuqaqurt2@gmail.com>
2023-07-21 11:09:04 +0300

    token

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("./database/database.js").connect();
+const auth = require("./middleware/auth");
 const express = require("express")
 const router = require("./routes/index");
 const app = express()
@@ -13,6 +14,10 @@ app.get("/", (req, res) => {
 
 // Register the application main router
 app.use("/api", router);
+
+app.post("/api/hello", auth, (req, res) => {
+     res.status(200).send("Hello 🙌 ");
+    });
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}!`)
