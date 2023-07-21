9b6f7cbf44e42942ba087ff13fff2b61e49f65a3
QTUQ <tuqaqurt2@gmail.com>
2023-07-21 13:50:55 +0300

    Authentication Service

---
+++ frontend/src/services/auth.service.js
@@ -0,0 +1,43 @@
+import axios from "axios"; // http client
+const API_URL = "http://localhost:40000/api"; //the API endpoint to communicate with the server
+
+// handles the signup http request to add a new user to the database
+const signup = ({firstName, lastName, username, email, password}) => {
+    return axios.post(`＄{API_URL}/signup/`, {
+        firstName,
+        lastName,
+        username,
+        email,
+        password,
+    });
+};
+
+// handles the login http request to access  user profile
+// tha data needed for each user is the username or email along with the password
+const login = ({emailOrUsername, password}) => {
+    return axios
+    .post(`＄{API_URL}/login/`, {emailOrUsername, password})
+    // if successfuly logged in, store the user data, including the token, in the localstorge
+    .then((res) => {
+        localStorage.setItem("user", JSON.stringify(res.data));
+    })
+};
+
+// hamdles the logout
+const logout = () => {
+    localStorage.removeItem("user");
+};
+
+// get the user from the localstorge
+const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem("user"));
+};
+
+const AuthService = {
+    signup,
+    login,
+    logout,
+    getCurrentUser,
+}
+
+export default AuthService;
