8755f4338b3b1ecca8905413007199b236592176
QTUQ <tuqaqurt2@gmail.com>
2023-07-23 17:35:21 +0300

    add /upload route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,13 @@
 const router = require("express").Router(); // Ge the router instance of Express
 const userController = require("../controllers/user"); // Get all exported functions in the user controller
+const auth = require("../middleware/auth");
+const { upload } = require("../middleware/multer");
 
 // Map the `signup` request to the signup function
 router.post("/signup", userController.signup);
 
 router.get("/verify/:confirmationToken", userController.verifyEmail);
 router.post("/login", userController.login);
+router.post("/upload", auth, upload.single("file"), fileController.upload);
 
 module.exports = router;
